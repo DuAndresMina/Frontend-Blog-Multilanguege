7a00ffc07eddd3e15a60754d5c730b86cae8e027
Duvan Mina <duvan.andres312@gmail.com>
2025-05-27 17:23:26 -0500

    Fix section random posts

--- src/components/RandomPostsSection.jsx
+++ src/components/RandomPostsSection.jsx
@@ -110,7 +110,7 @@ export default function RandomPostsSection() {
               {imgUrl && (
                 <div className="relative h-40 overflow-hidden">
                   <img 
-                    src={`${API_BASE_URL_img}${imgUrl}`} 
+                    src={imgUrl} 
                     alt={attr.Titulo} 
                     className="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300"
                   />
